c748c4b0930408b92c52443115243475f25e034f
Georgey <geobro2310@gmail.com>
2021-03-11 22:25:10 +0530

    Add dynamic blog routes and content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,36 @@
+import { InferGetStaticPropsType } from 'next'
+import Link from 'next/link'
 import Page from '@layouts/page'
+import { getPosts } from '@shared/get-posts'
 
-export default function Home() {
-  return <Page />
+export default function Home({
+  Posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Page>
+      <ul>
+        {Posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/${post.slug}`}>
+              <a>{post.frontMatter.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Page>
+  )
+}
+
+export const getStaticProps = async () => {
+  const Posts = await getPosts('./posts')
+  const allMdx = Posts.map(({ slug, frontMatter }) => ({
+    slug,
+    frontMatter,
+  }))
+
+  return {
+    props: {
+      Posts: allMdx,
+    },
+  }
 }
